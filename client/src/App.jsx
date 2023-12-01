0d486c86fe6e08ad75f194c8b5dd45e74b1feab6
Chandani Kumari <chandaanikumari92@gmail.com>
2023-12-01 18:21:48 +0530

    home page with carousel implementation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,38 +5,21 @@ import CategorywiseRecipeList from './components/CategorywiseRecipeList';
 import {BrowserRouter, Link, Routes, Route} from 'react-router-dom';
 import useApplicationData from './hooks/useApplicationData';
 import NavBar from './components/NavBar';
+import HomePage from './components/HomePage';
 import './App.css';
 
 
 export default function App() {
   const {categories,  recipes,error,categoryRecipes,  fetchItems,getCategoryId} = useApplicationData();
 
-//   return (
-//     <div className="App">
-//       <h1>My Anything List</h1>
-
-//       <Status status={status} error={error} />
-
-//       <button onClick={fetchItems}>Reload</button>
-//       <input type="text" value={text} onChange={e => setText(e.target.value)} />
-//       <button onClick={addFriend}>Add</button>
-
-//       <DataList friends={data} deleteItem={deleteItem} />
-//     </div>
-//   );
-const AllRecipes = () => <div>All Recipes Page Content</div>;
-const FavoriteRecipes = () => <div>Favorite Recipes Page Content</div>;
-
-const App = () => {
   return (
     <div className="App">
     
     <BrowserRouter>
         <NavBar />
-
         <Routes>
-{/*        
-          <Route path="/" element={<Home />} /> */}
+       
+          <Route path="/" element={<HomePage />} />
           <Route path="/categories" element={<CategoriesList categories={categories} getCategoryId={getCategoryId}/>} />
           <Route path="/recipes" element={<RecipeList recipes={recipes} />} />
           <Route path="/category/:categoryId" element={<CategorywiseRecipeList categoryRecipes={categoryRecipes} />}  />
@@ -47,6 +30,4 @@ const App = () => {
       
     </div>
   );
-};
-
-export default App;
+}
