7cc380c7d6f58c0e2deb621306267dbb7984d258
Bethanypaul989 <slayy.mayaa188@gmail.com>
2023-12-02 23:55:04 -0700

    login page

---
+++ client/src/components/login.jsx
@@ -0,0 +1,59 @@
+//~~LOGIN PAGE~~//
+
+import React, { useState } from 'react';
+import '../styles/styles/login.css'
+
+
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleLogin = () => {
+      // Add your login logic here
+      console.log('Logging in with:', { username, password });
+    };
+  
+    const handleCancel = () => {
+      // Add any cancel logic or redirection here
+      console.log('Login cancelled');
+    };
+  
+    return (
+      <div className="login-container">
+        <h2>Login</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+  
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+  
+          <div className="button-group">
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+            <button type="button" onClick={handleCancel} className="cancel-button">
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  };
+  
+  export default Login;
