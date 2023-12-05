184c2b9873b39e89a050bb5d7a9069130c929f9d
Bethanypaul989 <slayy.mayaa188@gmail.com>
2023-12-05 01:07:58 -0700

    updating

--- client/src/components/RecipeListItem.jsx
+++ client/src/components/RecipeListItem.jsx
@@ -2,14 +2,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 
-function RecipeListItem(props) {
-
-  const { name,description,instructions,calories,protein,carbs,image_url,ingredients_list } = props.recipe;
+const RecipeListItem = (props) => {
+  const { name, description, instructions, calories, protein, carbs, image_url, ingredients_list } = props.recipe;
   const imageUrl = `/${image_url}`;
+
+  const handleFavoriteClick = () => {
+    addToFavorites(props.recipe.id); 
+  };
+
   return (
     <div className="col">
       <div className="card border-light mb-3">
-        <div style={{ height: "400px", overflow: 'hidden' }}><img src={imageUrl} className="card-img-top" alt={name} /></div>
+        <div style={{ height: "400px", overflow: 'hidden' }}>
+          <img src={imageUrl} className="card-img-top" alt={name} />
+        </div>
 
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
@@ -22,23 +28,21 @@ function RecipeListItem(props) {
             <li className="list-group-item">
               <strong>Ingredients:</strong> {ingredients_list}
             </li>
-            <button onClick={() => handleFavouriteClick(favourite.id)}>
-  <FontAwesomeIcon icon={faHeart} />
-</button>
+            <button onClick={handleFavoriteClick}>
+              <FontAwesomeIcon icon={faHeart} />
+            </button>
 
-          <li className="list-group-item">
+            <li className="list-group-item">
               <strong>Nutritional Information:</strong>
-              {carbs && <p>Carbs:  {carbs}</p>}
-              { calories && <p>Calories:{calories}</p>}
-             {protein && <p>Protein:{protein}</p>}
+              {carbs && <p>Carbs: {carbs}</p>}
+              {calories && <p>Calories: {calories}</p>}
+              {protein && <p>Protein: {protein}</p>}
             </li>
           </ul>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default RecipeListItem;
-
-
--- client/src/components/favouritePage.jsx
+++ client/src/components/favouritePage.jsx
@@ -2,6 +2,42 @@ import React from 'react';
 import Favourite from './favourite';
 
  
+const FavoritesPage = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      try {
+        const response = await fetch('/api/favorites/getFavorites');
+        if (!response.ok) {
+          throw new Error('Failed to fetch favorites');
+        }
+
+        const data = await response.json();
+        setFavorites(data.favorites);
+      } catch (error) {
+        console.error('Error fetching favorites:', error.message);
+        // Handle error or display a user-friendly message
+      }
+    };
+
+    // Call the fetchFavorites function
+    fetchFavorites();
+  }, []); // Empty dependency array ensures the effect runs only once on component mount
+
+  return (
+    <div>
+      <h2>Your Favorites</h2>
+      <ul>
+        {favorites.map((favorite) => (
+          <li key={favorite.id}>{favorite.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+
 
 const FavouritePage = () => {
   return (
